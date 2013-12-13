56c1d54d81af8e141d5dba8133d52560e17d437b
Jiahao Li <isundaylee.reg@gmail.com>
2013-12-12 20:42:43 -0500

    Tabs to spaces.

--- lib/simple_cache.rb
+++ lib/simple_cache.rb
@@ -1,42 +1,41 @@
 require "simple_cache/version"
 
 module SimpleCache
-  # Your code goes here...
 
   class Cacher
-  	require 'fileutils'
+    require 'fileutils'
 
-  	def initialize(cache_dir, options = {})
-  		@cache_dir = File.expand_path(cache_dir)
+    def initialize(cache_dir, options = {})
+      @cache_dir = File.expand_path(cache_dir)
       @store_urls = options[:store_urls] || false
 
       @urls = {} if @store_urls
 
-  		FileUtils.mkdir_p(@cache_dir)
-  	end
+      FileUtils.mkdir_p(@cache_dir)
+    end
 
-  	def retrieve(url, key, options = {})
-  		# Defaults to hide caching progress.
-  		show_progress = options[:show_progress] || false
+    def retrieve(url, key, options = {})
+      # Defaults to hide caching progress.
+      show_progress = options[:show_progress] || false
 
-  		# If expiration is set to nil, the cache never expires.
-  		# Otherwise, cache expires after expiration seconds.
-  		expiration = options[:expiration] || nil
+      # If expiration is set to nil, the cache never expires.
+      # Otherwise, cache expires after expiration seconds.
+      expiration = options[:expiration] || nil
 
-  		if cached?(key)
-  			if expired?(key, expiration)
-  				clear_cache(key)
-  				# Call self to re-retrieve. 
-  				retrieve(url, key, options)
-  			else
-  				retrieve_cache(key)
-  			end
-  		else
-  			download_to_cache(url, key)
+      if cached?(key)
+        if expired?(key, expiration)
+          clear_cache(key)
+          # Call self to re-retrieve. 
+          retrieve(url, key, options)
+        else
+          retrieve_cache(key)
+        end
+      else
+        download_to_cache(url, key)
         @urls[key] = url if @store_urls
-  			retrieve_cache(key)
-  		end
-  	end
+        retrieve_cache(key)
+      end
+    end
 
     def retrieve_by_key(key, options = {})
       show_progress = options[:show_progress] || false
@@ -58,43 +57,43 @@ module SimpleCache
       end
     end
 
-  	private
-  		def cached?(key)
-  			File.exists?(cache_path(key))
-  		end
+    private
+      def cached?(key)
+        File.exists?(cache_path(key))
+      end
 
-  		def expired?(key, expiration)
-  			expiration && (File.mtime(cache_path(key)) < Time.now - expiration)
-  		end
+      def expired?(key, expiration)
+        expiration && (File.mtime(cache_path(key)) < Time.now - expiration)
+      end
 
-  	  def clear_cache(key)
-  	  	FileUtils.rm_f(cache_path(key))
-  	  end
+      def clear_cache(key)
+        FileUtils.rm_f(cache_path(key))
+      end
 
-  	  def retrieve_cache(key)
-  	  	File.open(cache_path(key)).read
-  	  end
+      def retrieve_cache(key)
+        File.open(cache_path(key)).read
+      end
 
-  	  def download_to_cache(url, key)
-  	  	# First download to a temporary file. 
-  	  	download(url, tmp_path(key))
+      def download_to_cache(url, key)
+        # First download to a temporary file. 
+        download(url, tmp_path(key))
 
-  	  	# Rename it. 
-  	  	FileUtils.mv(tmp_path(key), cache_path(key))
-  	  end
+        # Rename it. 
+        FileUtils.mv(tmp_path(key), cache_path(key))
+      end
 
-  	  def download(url, path)
-  	  	require 'open-uri'
-  	  	File.write(path, open(url).read)
-  	  end
+      def download(url, path)
+        require 'open-uri'
+        File.write(path, open(url).read)
+      end
 
-  		def cache_path(key)
-  			File.join(@cache_dir, key)
-  		end
+      def cache_path(key)
+        File.join(@cache_dir, key)
+      end
 
-  		def tmp_path(key)
-  			File.join(@cache_dir, "#{key}.tmp")
-  		end
+      def tmp_path(key)
+        File.join(@cache_dir, "#{key}.tmp")
+      end
 
   end
 end
